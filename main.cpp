ca0cea1112c1403954a9e478a49661cb2aae16b9
PigeonGaming <wassimdzd@gmail.com>
2022-09-06 10:49:57 +0100

    noice

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
 #include <raylib.h>
-#include <iostream>
-#include <string> 
-
-using namespace std;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -20,60 +16,60 @@ const int screenHeight = 500;
 
 int score = 0;
 
-float square1Edges[4];
-float square2Edges[4];
+float player1edges[4];
+float player2edges[4];
 float ballEdges[4];
 
-bool play1 = true;
-bool play2 = true;
+bool player1 = true;
+bool player2 = true;
 
 bool dead = false;
 
 const int min = 0;
 const int max = 1;
 
-Vector2 squarePosition;
-Vector2 squarePosition2;
+Vector2 Player;
+Vector2 Player2;
 Vector2 Ball;
 Vector2 BallVec;
 
 void UpdateEdges(){
-  square1Edges[0] = squarePosition.x;
-  square1Edges[1] = squarePosition.x + playerWidth; 
-  square1Edges[2] = squarePosition.y; 
-  square1Edges[3] = squarePosition.y + playerHeight; 
+  player1edges[0] = Player.x;
+  player1edges[1] = Player.x + playerWidth; 
+  player1edges[2] = Player.y; 
+  player1edges[3] = Player.y + playerHeight; 
 
-  square2Edges[0] = squarePosition2.x;
-  square2Edges[1] = squarePosition2.x + playerWidth; 
-  square2Edges[2] = squarePosition2.y; 
-  square2Edges[3] = squarePosition2.y + playerHeight; 
+  player2edges[0] = Player2.x;
+  player2edges[1] = Player2.x + playerWidth; 
+  player2edges[2] = Player2.y; 
+  player2edges[3] = Player2.y + playerHeight; 
 }
 
 void CollisionPlayerWindow(){
-  if (square1Edges[2] < 0){
-    play1 = false;
-    squarePosition.y += SPEED;
+  if (player1edges[2] < 0){
+    player1 = false;
+    Player.y += SPEED;
   }else{
-    play1 = true;
+    player1 = true;
   }
-  if (square1Edges[3] > screenHeight){
-    play1 = false;
-    squarePosition.y -= SPEED;
+  if (player1edges[3] > screenHeight){
+    player1 = false;
+    Player.y -= SPEED;
   }else{
-    play1 = true;
+    player1 = true;
   }
 
-  if (square2Edges[2] < 0){
-    play2 = false;
-    squarePosition2.y += SPEED;
+  if (player2edges[2] < 0){
+    player2 = false;
+    Player2.y += SPEED;
   }else{
-    play2 = true;
+    player2 = true;
   }
-  if (square2Edges[3] > screenHeight){
-    play2 = false;
-    squarePosition2.y -= SPEED;
+  if (player2edges[3] > screenHeight){
+    player2 = false;
+    Player2.y -= SPEED;
   }else{
-    play2 = true;
+    player2 = true;
   }
 }
 
@@ -90,16 +86,16 @@ void BallCollision(){
   // if (Ball.x - BALLRADIUS < square2Edges[1] + 4){cout << "ouch" << endl;}
 
   // cout << Ball.x - BALLRADIUS << "|" << square2Edges[1] + 4 << endl;
-  if (Ball.x - squarePosition2.x > 5 && Ball.x - BALLRADIUS < square2Edges[1] + 4)
+  if (Ball.x - Player2.x > 5 && Ball.x - BALLRADIUS < player2edges[1] + 4)
   {
-    if (Ball.y > square2Edges[2] && Ball.y < square2Edges[3]){
-      BallVec.x = -BallVec.x;
+    if (Ball.y > player2edges[2] && Ball.y < player2edges[3]){
+      BallVec.x = -BallVec.x * 1.03;
       score++;
     } 
   }
-  if (squarePosition.x - Ball.x < 5 || Ball.x + BALLRADIUS > square1Edges[0] - 1)
+  if (Player.x - Ball.x < 5 || Ball.x + BALLRADIUS > player1edges[0] - 1)
   {
-    if (Ball.y > square1Edges[2] && Ball.y < square1Edges[3])
+    if (Ball.y > player1edges[2] && Ball.y < player1edges[3])
     {
       BallVec.x = -BallVec.x ;
       score++;
@@ -107,77 +103,77 @@ void BallCollision(){
   }
 }
 
-int main(void)
-{
-  // Initialization
-  //--------------------------------------------------------------------------------------
- 
-  InitWindow(screenWidth, screenHeight, "shitty pong");
+void updateBallPos(){
+  if(!dead){
+    Ball.x += BallVec.x * BALLSPEED;
+    Ball.y += BallVec.y * BALLSPEED;
+    BallCollision();
+    }
+}
 
-  squarePosition2 = { 6.0 , (float)screenHeight/2 };
-  squarePosition = { (float)screenWidth - playerWidth - 6.0, (float)screenHeight/2 };
+void PlayerMovement(){
+   if (player1 && !dead)
+    {
+      if (IsKeyDown(KEY_W)) Player2.y -= SPEED;
+      if (IsKeyDown(KEY_S)) Player2.y += SPEED;
+    }
+    if (player2 && !dead)
+    {
+      if (IsKeyDown(KEY_UP)) Player.y -= SPEED;
+      if (IsKeyDown(KEY_DOWN)) Player.y += SPEED;
+    }
+}
+
+void Draw(){
+  BeginDrawing();
+    ClearBackground(BLACK);
+
+    DrawCircle(Ball.x, Ball.y , BALLRADIUS, WHITE);
+    DrawRectangle(Player.x, Player.y, playerWidth, playerHeight , WHITE);
+    DrawRectangle(Player2.x, Player2.y, playerWidth, playerHeight , WHITE);
+
+    DrawText(TextFormat("Score: %08i", score), screenWidth/2 , 40, 20, WHITE);
+    if (dead){DrawText("u lost", screenWidth/2, screenHeight/2, 40, WHITE);}
+  EndDrawing();
+}
+
+void init(){
+  InitWindow(screenWidth, screenHeight, "shitty pong");
+  SetTargetFPS(60);       
+  
+  Player2 = { 6.0 , (float)screenHeight/2 };
+  Player = { (float)screenWidth - playerWidth - 6.0, (float)screenHeight/2 };
 
   Ball = { (float)screenWidth/2, (float)screenHeight/2 };
 
-  float BallX = GetRandomValue(-10, 10);  
-  float BallY = GetRandomValue(-7, 7);  
+  float BallX = GetRandomValue(-1, 1);  
+  float BallY = GetRandomValue(-1, 1);  
 
-  BallVec = { BallX / 5 , BallY / 5 };        
+  BallVec = { BallX , BallY };    
 
-  SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+}
+
+int main(void)
+{
+  // Initialization
+
+  init();
 
   // Main game loop
   while (!WindowShouldClose())    // Detect window close button or ESC key
   {
+    // Update
     
     UpdateEdges();
     CollisionPlayerWindow();
+    updateBallPos();
+    PlayerMovement();
 
-    if(!dead){
-      Ball.x += BallVec.x * BALLSPEED;
-      Ball.y += BallVec.y * BALLSPEED;
-      BallCollision();
-    }
+    Draw();
     
-    // Update
-    //----------------------------------------------------------------------------------
-    if (play1 && !dead)
-    {
-      if (IsKeyDown(KEY_W)) squarePosition2.y -= SPEED;
-      if (IsKeyDown(KEY_S)) squarePosition2.y += SPEED;
-    }
-    if (play2 && !dead)
-    {
-      if (IsKeyDown(KEY_UP)) squarePosition.y -= SPEED;
-      if (IsKeyDown(KEY_DOWN)) squarePosition.y += SPEED;
-    }
-
-    
-
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-      ClearBackground(BLACK);
-
-
-        DrawCircle(Ball.x, Ball.y , BALLRADIUS, WHITE);
-
-        DrawRectangle(squarePosition.x, squarePosition.y, playerWidth, playerHeight , WHITE);
-        DrawRectangle(squarePosition2.x, squarePosition2.y, playerWidth, playerHeight , WHITE);
-
-        DrawText(TextFormat("Score: %08i", score), screenWidth/2 , 40, 20, WHITE);
-        if (dead){DrawText("u lost", screenWidth/2, screenHeight/2, 40, WHITE);}
-      EndDrawing();
-      //----------------------------------------------------------------------------------
   }
 
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
   CloseWindow();        // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
 
   
   return 0;
